3a492866a4254e34b127001d49914b8965bb664b
James Bunton <jamesbunton@delx.net.au>
2015-05-24 22:14:45 +1000

    Clearer error message when unencrypted stream is not available

--- brightcove.py
+++ brightcove.py
@@ -25,6 +25,8 @@ class BrightcoveVideoNode(Node):
         })
         doc = grab_json(desc_url, 3600)
         video_url = doc["HLSURL"]
+        if not video_url:
+            raise Exception("No HLS stream available for: " + self.title)
 
         filename = self.title + ".ts"
         return download_hls(filename, video_url)
